93d865d3700e81914cadbeeea645aa24ce74bedc
gergoke68 <50143223+gergoke68@users.noreply.github.com>
2025-09-30 12:38:21 +0200

    FIx: typos

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use("/users", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
